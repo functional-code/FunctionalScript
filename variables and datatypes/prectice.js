064158e654eff335f176054f3bb453fa9d12f5cb
functional-code <ndivya945@gmail.com>
2025-12-04 00:45:03 +0530

    questions are solved

---
+++ variables and datatypes/prectice.js
@@ -0,0 +1,25 @@
+// create a variable of type string and try to add a number to it
+ let a = "shrinath"
+    let b = 90;
+{ 
+   
+    console.log(a+b);
+}
+
+//use type of oprator 
+{
+    console.log(typeof (a+b));
+}
+
+//mini dictionary 
+{
+    let functionaldic ={
+    name:"shrinath",
+    rollcall : 73,
+    place : "ponda",
+    devise: "mac mini",
+    frame: "golden"
+
+    }
+    console.log(functionaldic)
+}
