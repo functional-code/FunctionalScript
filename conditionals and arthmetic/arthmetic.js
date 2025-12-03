eff89991b0c59d51c0f170b433af27109bc85bc0
functional-code <ndivya945@gmail.com>
2025-12-04 01:01:12 +0530

    arthimathic half

---
+++ conditionals and arthmetic/arthmetic.js
@@ -0,0 +1,14 @@
+let a= 10;
+let b= 2;
+
+//basic
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+//assignment oprators
+a += b;  // a= a+b;
+console.log(a)
---
+++ conditionals and arthmetic/functionalelse.js
@@ -0,0 +1,14 @@
+console.log("hello welcome to functionalcondition");
+
+let age = 17;
+let grace = 2;
+
+
+if((age + grace)>18){
+    console.log("you can drive");
+
+}
+
+else{
+    console.log("driving unfunctional")
+}
