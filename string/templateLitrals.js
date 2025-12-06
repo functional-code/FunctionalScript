fe616dab6ac14f9080730b2415602d6a337efadf
functional-code <ndivya945@gmail.com>
2025-12-06 20:44:53 +0530

    this is template push

---
+++ string/templateLitrals.js
@@ -0,0 +1,5 @@
+let name = "FunctionalTanishq"
+let hername = "FunctionalAldria"
+
+console.log("His name is "+name+" and her name was "+ hername);
+console.log(`His name is ${name} and her name was ${hername}`);
