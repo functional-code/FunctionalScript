a0f1e4bc04efd72046a9558add1938793c901bb0
functional-code <ndivya945@gmail.com>
2025-12-07 11:05:01 +0530

    this is string push

---
+++ string/slice.replace.js
@@ -0,0 +1,12 @@
+let name ="functionalloop";
+let print = "peint";
+
+//slice
+console.log(name.slice(3,4));
+console.log(name.slice(3));
+
+//replace
+console.log(name.replace("loop","print"));
+
+//contat
+console.log(name.concat(print,"this is nit"));
