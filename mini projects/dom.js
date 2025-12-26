c66a365dea2611b7c843f73653482a107dd18c74
functional-code <ndivya945@gmail.com>
2025-12-26 22:02:36 +0530

    this is a inserting push

--- mini projects/dom.js
+++ mini projects/dom.js
@@ -1,11 +1,27 @@
 
-function selector(Math.random){
-
+function selector(a, element){
+    if(a<0.2){
+        element.style.backgroundColor ="green"
+    }
+    else if(a>0.2 && a<0.4){
+        element.style.backgroundColor ="black"
+    }
+    else if(a>0.4 && a<0.6){
+        element.style.backgroundColor ="blue"
+    }
+    else if(a>0.6 && a<0.8){
+        element.style.backgroundColor ="grey"
+    }
+    else if(a>0.8 && a<1){
+        element.style.backgroundColor ="red"
+    }
 }
 
 
 document.querySelectorAll(".box").forEach(element => {
-    element.style.backgroundColor ="green"
+    x = Math.random();
+    // 
+    selector(x,element)
 });
 
 
