344d394fc0625c41ca309ecc37748ad95e555763
functional-code <ndivya945@gmail.com>
2025-12-29 11:54:20 +0530

    Uncomment code for callback and script loading

--- callback and promises/callback.js
+++ callback and promises/callback.js
@@ -1,14 +1,14 @@
-// console.log("this is shrinidh part 2")
-// const callback= (arg)=>{
-//   console.log(arg)
-// }
+console.log("this is shrinidh part 2")
+const callback= (arg)=>{
+  console.log(arg)
+}
 
-// const loadScript = (src,callback) => {
-//   let sc =document.createElement("script")
-//   sc.src =src;
-//   sc.onload = callback("Harry");
-//   document.head.append(sc)
-// }
+const loadScript = (src,callback) => {
+  let sc =document.createElement("script")
+  sc.src =src;
+  sc.onload = callback("Harry");
+  document.head.append(sc)
+}
 
-// loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
 
